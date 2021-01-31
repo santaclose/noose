96788175080199a58d564761f6a17e7e51b7e5e3
Santiago <san@san.san>
2021-01-31 17:36:21 +0100

    cleaning

--- src/interface/uiHelp.h
+++ src/interface/uiHelp.h
@@ -6,27 +6,21 @@
 
 namespace uiHelp
 {
+	const std::string helpUrl = "http://sntcls.ml/nooseHelp";
 	inline void displayHelp()
 	{
-		std::cout << "[UI] Opening help HTML\n";
+		std::cout << "[UI] Opening help url\n";
 		std::string command;
-		std::string localPath;
 #ifdef NOOSE_PLATFORM_LINUX
 		command = "xdg-open ";
-		localPath = "assets/help/help.html";
-		//system("xdg-open " + utils::getProgramDirectory() + "help/help.html");
 #endif
 #ifdef NOOSE_PLATFORM_MACOS
 		command = "open ";
-		localPath = "assets/help/help.html";
-		//system("open " + utils::getProgramDirectory() + "help/help.html");
 #endif
 #ifdef NOOSE_PLATFORM_WINDOWS
-		command = "start \"\" ";
-		localPath = "assets\\help\\help.html";
-		//system("start " + utils::getProgramDirectory() + "help\\help.html");
+		command = "explorer ";
 #endif
-		command += "\"" + utils::getProgramDirectory() + localPath + "\"";
+		command += "\"" + helpUrl + "\"";
 		system(command.c_str());
 	}
 }
