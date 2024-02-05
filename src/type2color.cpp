4e52310ea42627ffae2f7974fe9d2af8718f19da
Santiago <san@san.san>
2024-02-05 19:30:13 +0100

    replace unordered map with switch for type to color conversion

--- src/type2color.cpp
+++ src/type2color.cpp
@@ -1,22 +1,23 @@
 #include "type2color.h"
-#include <unordered_map>
 #include "types.h"
-
-namespace type2color {
-
-	std::unordered_map<int, int> mapping =
-	{
-		{NS_TYPE_INT, 0x4ca4fbff},
-		{NS_TYPE_FLOAT, 0xee1133ff},
-		{NS_TYPE_VECTOR2I, 0x56957aff},
-		{NS_TYPE_COLOR, 0xaa7700ff},
-		{NS_TYPE_IMAGE, 0x00bc44ff},
-		{NS_TYPE_STRING, 0xbc007aff},
-		{NS_TYPE_FONT, 0x7d7d7dff}
-	};
-}
 
 const int type2color::get(int type)
 {
-	return mapping[type];
+	switch (type)
+	{
+	case NS_TYPE_INT:
+		return 0x4ca4fbff;
+	case NS_TYPE_FLOAT:
+		return 0xee1133ff;
+	case NS_TYPE_VECTOR2I:
+		return 0x56957aff;
+	case NS_TYPE_COLOR:
+		return 0xaa7700ff;
+	case NS_TYPE_IMAGE:
+		return 0x00bc44ff;
+	case NS_TYPE_STRING:
+		return 0xbc007aff;
+	case NS_TYPE_FONT:
+		return 0x7d7d7dff;
+	}
 }
